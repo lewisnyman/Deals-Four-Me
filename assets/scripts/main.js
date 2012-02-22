46e311ec3e64c179d4276243fd905b69e935af02
Lewis Nyman <hello@lewisnyman.co.uk>
2012-02-22 17:34:59 +0000

    Hardcoded the url as a quick fix

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,7 +21,7 @@
  var geo = false;
  var lat; 
  var lng;
- var currenturl = window.location.toString();
+ var currenturl = 'www.dealsfourme.com';
  
  templates = {
   category : "<div class='category {{times}}-times'>{{name}} - {{times}} times</div>",
